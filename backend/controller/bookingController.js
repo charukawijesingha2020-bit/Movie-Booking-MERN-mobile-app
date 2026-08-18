c2171cde54c9db0844f7a6cf334cd4498ef2d0cd
Oshan Charuka <charukawijesingha2020@gmail.com>
2026-08-18 22:39:05 +0530

    Remove unnecessary blank lines in bookingController

--- backend/controller/bookingController.js
+++ backend/controller/bookingController.js
@@ -1,8 +1,6 @@
 const Booking = require('../model/Booking');
 const Screening = require('../model/Screening');
 
-
-
 // @route POST /api/bookings  [User]
 const createBooking = async (req, res) => {
   try {
